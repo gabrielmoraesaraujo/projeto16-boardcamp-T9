2bc3fada27585ad5c5a6c2eda9a27d1da894c034
Gabriel Moraes <gmoraes.mat@gmail.com>
2023-02-13 15:13:12 -0300

    Criação do controller categories

--- src/controllers/categories.Controllers.js
+++ src/controllers/categories.Controllers.js
@@ -1,8 +1,13 @@
+import { db } from "../database/database.js";
+
+
 export async function create(req, res){
 
-    const categories = res.locals.categories;
+    const {name} = res.locals.categories;
 
     try{
+        await db.query("INSERT INTO categories (name) VALUES ($1)", [name]);
+        res.sandStatus(201);
 
     } catch(err){
         res.status(500).sand(err.massage);
